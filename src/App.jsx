d829961fd47cfcd95381d66b7763b932a24f3bd8
1ynxa <tktk.kanako925@gmail.com>
2024-09-26 00:03:11 +0900

    取り消し

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,6 @@ const App=()=> {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-         <p>コミット用</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
