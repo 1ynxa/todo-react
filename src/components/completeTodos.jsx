0c3a77f3151daffca3289b285d4443fff9af3c8e
1ynxa <tktk.kanako925@gmail.com>
2024-09-29 15:35:02 +0900

    コンポーネント化

---
+++ src/components/completeTodos.jsx
@@ -0,0 +1,20 @@
+export const CompleteTodos = (props) => {
+  const { todos, onClickBack } = props;
+  return (
+    <div className="complete-area">
+      <p className="title">完了のTodo</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button onClick={() => onClickBack(index)}>戻す</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
